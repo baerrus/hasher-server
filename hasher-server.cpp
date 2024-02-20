0214fb225265516575a7c8a345c0342e76a02483
Vlad Troyanker <vtroyanker@roku.com>
2024-02-19 17:48:39 -0800

    improve modularity

---
+++ hasher-server.cpp
@@ -0,0 +1,109 @@
+//
+// Copyright (c) 2024 Vlad Troyanker
+//
+#include <errno.h>
+
+#include "hasher-server.h"
+
+
+ClientSession::ClientSession(tcp::socket socket, asio::thread_pool& compute)
+    : socket_(io_context_)
+    , strand_(compute.executor())
+    , compute_(compute)
+{
+    // must transfer socket ownership since client connection
+    // operates on a different io_context from the acceptor
+    socket_.assign(tcp::v4(), socket.release());
+}
+
+void ClientSession::start()
+{
+    try {
+        std::cout << "New client: " << socket_.remote_endpoint()
+                  << " threadId: " << std::this_thread::get_id() << std::endl;
+        do_read();
+        io_context_.run();
+
+        std::cout << "Client thread ended: " << std::this_thread::get_id() << std::endl;
+    } catch (std::exception& e) {
+        std::cerr << "** client exception: " << e.what() << "\n";
+    }
+}
+
+void ClientSession::do_read()
+{
+    auto buffer = make_buffer(max_read_size);
+    if (socket_.is_open())
+        socket_.async_read_some(asio::buffer(buffer->data(), max_read_size),
+            [this, buffer, self = shared_from_this()](std::error_code ec, std::size_t length) {
+                if (!ec) {
+                    buffer->resize(length);
+                    // asio::post(compute_, asio::bind_executor(strand_, [this, self, buffer](){
+                    hasher_.work(buffer, [self](Buffer buffer) {
+                        self->do_write(buffer);
+                    }); // end of work callback
+                    //})); // end of post handler
+                    self->do_read();
+                } else if (ec == asio::error::misc_errors::eof) {
+
+                    std::cout << "Client disconnected: " << socket_.remote_endpoint() << std::endl;
+                    socket_.shutdown(asio::ip::tcp::socket::shutdown_receive);
+                } else {
+                    std::cerr << "read error: " << ec << std::endl;
+                    self->do_close();
+                }
+            });
+}
+
+void ClientSession::do_write(Buffer buffer)
+{
+    auto self(shared_from_this());
+    if (socket_.is_open())
+        asio::async_write(socket_, asio::buffer(buffer->data(), buffer->size()),
+            [self, buffer](std::error_code ec, std::size_t /*length*/) {
+                if (ec) {
+                    std::cerr << "write error: " << ec << std::endl;
+                    self->do_close();
+                }
+            });
+}
+
+void ClientSession::do_close()
+{
+    socket_.close();
+}
+
+TcpServer::TcpServer(short port)
+    : acceptor_(io_context_, tcp::endpoint(tcp::v4(), port))
+    , socket_(io_context_)
+    , io_pool_(16)
+    , compute_pool_(10)
+{
+    std::cout << "Starting server on " << tcp::endpoint(tcp::v4(), port)
+              << std::endl;
+}
+
+TcpServer::~TcpServer()
+{
+    std::cout << "Stopping server on " << acceptor_.local_endpoint() << std::endl;
+    io_pool_.join();
+    compute_pool_.join();
+}
+
+void TcpServer::serve()
+{
+    do_accept();
+    io_context_.run();
+}
+
+void TcpServer::do_accept()
+{
+    acceptor_.async_accept(socket_, [this](std::error_code ec) {
+        if (!ec) {
+            asio::post(io_pool_, [this]() {
+                std::make_shared<ClientSession>(std::move(socket_), compute_pool_)->start();
+            });
+        }
+        do_accept();
+    });
+}
---
+++ hasher-server.h
@@ -0,0 +1,57 @@
+//
+// Copyright (c) 2024 Vlad Troyanker
+//
+#include <iostream>
+#include <thread>
+
+#include <asio/bind_executor.hpp>
+#include <asio/strand.hpp>
+#include <asio/thread_pool.hpp>
+#include <asio/ts/buffer.hpp>
+#include <asio/ts/internet.hpp>
+
+#include "buffer.h"
+#include "hasher-stream.h"
+
+
+using asio::ip::tcp;
+
+class ClientSession : public std::enable_shared_from_this<ClientSession> {
+public:
+    ClientSession(tcp::socket socket, asio::thread_pool& compute);
+
+    void start();
+
+private:
+    void do_read();
+
+    void do_write(Buffer buffer);
+
+    void do_close();
+
+    asio::io_context io_context_;
+    tcp::socket socket_;
+    HasherStream hasher_; // hash compute engine
+    asio::strand<asio::thread_pool::executor_type> strand_; // serialize all compute within a single client
+    asio::thread_pool& compute_; // @hasher_ executes on this pool
+
+    enum { max_read_size = 1024 };
+};
+
+class TcpServer {
+public:
+    TcpServer(short port);
+
+    ~TcpServer();
+
+    void serve();
+
+private:
+    void do_accept();
+
+    asio::io_context io_context_; // acceptor runs in this context
+    tcp::acceptor acceptor_;
+    tcp::socket socket_;
+    asio::thread_pool io_pool_; // client connections run here
+    asio::thread_pool compute_pool_; // client connections run here
+};
